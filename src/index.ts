9130a4fa9fb641c99b784267099158868941081e
Robbie Pitts <me@robbie-pitts.com>
2017-03-24 20:45:06 -0400

    Custom element initializer function

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default } from './initialize-custom-elements';
---
+++ src/initialize-custom-elements.ts
@@ -0,0 +1,23 @@
+import Application from '@glimmer/application';
+
+export default function initializeCustomElements(app: Application, customElementDefinitions: { name: string }[]): void {
+  customElementDefinitions.forEach(({ name }) => {
+    initializeCustomElement(app, name);
+  });
+}
+
+function initializeCustomElement(app: Application, name: string): void {
+  class GlimmerElement extends HTMLElement {
+    connectedCallback() {
+      let placeholder = document.createTextNode('');
+      let parent = this.parentNode;
+
+      parent.insertBefore(placeholder, this);
+      parent.removeChild(this);
+
+      app.renderComponent(name, parent, placeholder);
+    }
+  }
+
+  window.customElements.define(name, GlimmerElement);
+}
