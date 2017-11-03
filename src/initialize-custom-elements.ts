96080622dcfcc0d63e39ecc269a8f328ce4ed1d7
Matthew Beale <matt.beale@madhatted.com>
2017-11-02 17:37:26 -0700

    Add missing ()

--- src/initialize-custom-elements.ts
+++ src/initialize-custom-elements.ts
@@ -49,7 +49,7 @@ function assignAttributes(fromElement: Element, toElement: Element): void {
 
 function whenRendered(app, callback) {
   if (app['_rendering']) {
-    self.Promise.resolve.then(() => {
+    self.Promise.resolve().then(() => {
       whenRendered(app, callback);
     });
   } else {
